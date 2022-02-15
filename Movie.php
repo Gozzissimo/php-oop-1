a722efd7b3ae0ecd2a5cc48b7a1c30915fe3db93
Gozzissimo <fantini.nico@gmail.com>
2022-02-15 17:20:36 +0100

    mpa rating array

--- Movie.php
+++ Movie.php
@@ -13,11 +13,52 @@ class Movie {
     public $director;
     public $cast;
     public $genre;
-    public $rating;
+    public $score;
+    public $mpaRatingTable = [
+        [
+            'ratingCode' => 'G',
+            'meaning' => 'General Audiences',
+            'description' => 'All ages admitted. Nothing that would offend parents for viewing by children.',
+        ],
+        [
+            'ratingCode' => 'PG',
+            'meaning' => 'Parental Guidance Suggested',
+            'description' => 'Some material may not be suitable for children. Parents urged to give "parental guidance". May contain some material parents might not like for their young children.',
+        ],
+        [
+            'ratingCode' => 'PG-13',
+            'meaning' => 'Parents Strongly Cautioned',
+            'description' => 'Some material may be inappropriate for children under 13. Parents are urged to be cautious. Some material may be inappropriate for pre-teenagers.',
+        ],
+        [
+            'ratingCode' => 'R',
+            'meaning' => 'Restricted',
+            'description' => 'Under 17 requires accompanying parent or adult guardian. Contains some adult material. Parents are urged to learn more about the film before taking their young children with them.',
+        ],
+        [
+            'ratingCode' => 'NC-17',
+            'meaning' => 'Adults Only',
+            'description' => 'No One 17 and Under Admitted. Clearly adult. Children are not admitted.',
+        ],
+    ];
+
+    public function __construct($title, $year)
+    {
+        $this->title = $title;
+        $this->year = $year;
+    }
+
+    // public function ageWarning($yearRating)
+    // {
+    //     if ( ) {
+    //         # code...
+    //     }
+    // }
 }
 
-$movie = new Movie();
-$movie->title = 'Starship Troopers - Fanteria dello spazio';
+// movie-1
+$movie = new Movie('Starship Troopers', 1997);
+$movie->title = 'Starship Troopers';
 $movie->year = 1997;
 $movie->director = 'Paul Verhoeven';
 $movie->cast = [
@@ -30,9 +71,11 @@ $movie->genre = [
     'action',
     'sci-fi'
 ];
-$movie->rating = 7.2;
+$movie->score = 7.2;
+$movie->rating = 'R';
 
-$movie2 = new Movie();
+// movie-2
+$movie2 = new Movie('Tremors', 1990);
 $movie2->title = 'Tremors';
 $movie2->year = 1990;
 $movie2->director = 'Ron Underwood';
@@ -46,7 +89,8 @@ $movie2->cast = [
 $movie2->genre = [
     'horror',
 ];
-$movie2->rating = 7.1;
+$movie2->score = 7.1;
+$movie2->rating = 'PG-13';
 
 var_dump($movie);
 var_dump($movie2);
